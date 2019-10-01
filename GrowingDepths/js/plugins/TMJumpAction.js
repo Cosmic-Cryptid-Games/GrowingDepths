d2c3ff70e2891cba36e2b181cbef8aa8eccdec34
modelarious <michaelwilliamhackman@gmail.com>
2019-10-01 14:33:57 -0600

    gave _wallJumpSpeed to the character instead of having a floating variable

--- GrowingDepths/js/plugins/TMJumpAction.js
+++ GrowingDepths/js/plugins/TMJumpAction.js
@@ -1164,7 +1164,7 @@ function Game_Bullet() {
     this._canDash = true;
     this._friction = 0;
     this._moveSpeed = 0.05;
-    this._wallJumpSpeed = 3;
+    this._wallJumpSpeed = 0.12;
     this._jumpSpeed = 0.14;
     this._swimSpeed = 0.02;
     this._dashSpeedX = 0.1;
@@ -2277,8 +2277,7 @@ function Game_Bullet() {
 
   // 壁ジャンプの X 方向処理
   Game_Player.prototype.wallJump = function() {
-    wallJumpSpeed = 0.12;
-    this._vx = this._direction == 4 ? wallJumpSpeed : -wallJumpSpeed;
+    this._vx = this._direction == 4 ? this._wallJumpSpeed : -this._wallJumpSpeed;
     this.setDirection(this.reverseDir(this._direction));
     this.resetPeak();
   };
@@ -2432,7 +2431,7 @@ function Game_Bullet() {
       var characterIndex  = actor.characterIndex();
       var data = actor.actor();
       this._moveSpeed = +(data.meta['move_speed'] || 0.065);
-      this._wallJumpSpeed = +(data.meta['wall_jump_speed'] || 0.10);
+      this._wallJumpSpeed = +(data.meta['wall_jump_speed'] || 0.12);
       this._jumpSpeed = +(data.meta['jump_speed'] || 0.14);
       this._swimSpeed = +(data.meta['swim_speed'] || 0.02);
       this._ladderSpeed = +(data.meta['ladder_speed'] || 0.04);
